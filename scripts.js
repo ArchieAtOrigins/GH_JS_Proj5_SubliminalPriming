8fbf3ffcf99f26bcfc36b94145ca6d45398e6db2
ArchieAtOrigins <archie@originscreative.com>
2017-02-17 11:24:22 -0800

    Added setInterval so words now flash on the screen

--- scripts.js
+++ scripts.js
@@ -1,12 +1,25 @@
 // Next stop: Get setInterval working. ref: https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
 
 
-var wordArray = ["happy", "healthy", "tolerant", "patient", "motivated", "calm", "focused", "disciplined", "kind", "productive", "learned", "confident", "positive", "action", "virtuous", "Thrive", "content", "smile", "mindful", "grateful", "persistent", "fortitude", "resilient", "faith", "inspired"];
+var wordArray = ["happy", "healthy", "tolerant", "patient", "motivated", "calm", "focused", "disciplined", "kind", "productive", "learned", "confident", "positive", "action", "virtuous", "Thrive", "content", "smile", "mindful", "grateful", "persistent", "fortitude", "resilient", "faith", "inspired", "love", "mastery", "excellence"];
+
+var int;
+
+function startRoll() {
+	int = setInterval(randomWord, 150);
+}
 
 function randomWord() {
+	var target = document.getElementById('message')
 	var wordCount = wordArray.length;
 	var spWord = wordArray[Math.floor(Math.random()*wordCount)];
+	target.innerHTML=spWord;
 	return spWord;
 }
-setInterval(randomWord, 100);
-var message = document.getElementById('message').textContent = randomWord();
+
+function stopRoll() {
+	clearInterval(int);
+}
+
+
+
