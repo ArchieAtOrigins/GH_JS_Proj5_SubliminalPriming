55f9c22d4af47442fb95d4fa9c90676ab1a1f9f4
ArchieAtOrigins <archie@originscreative.com>
2017-05-25 11:27:12 -0700

    Removed -onclick- from start and stop buttons and added in event handlers for more unubstrusive JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-// Next stop: Get setInterval working. ref: https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+// TODO's: Hitting start shortens the interval between new words. Needs fixing.
 
 
 var wordArray = ["relaxed", "happy", "healthy", "tolerant", "patient", "motivated", "calm", "focused", "disciplined", "kind", "productive", "learned", "confident", "positive", "action", "virtuous", "thrive", "content", "smile", "mindful", "grateful", "persistent", "fortitude", "resilient", "faith", "inspired", "love", "mastery", "excellence", "wonderful", "wise", "joy"];
 
 var int;
+
+document.getElementById('start').addEventListener('click', startRoll);
+document.getElementById('stop').addEventListener('click', stopRoll);
 
 function startRoll() {
 	int = setInterval(randomWord, 150);
@@ -14,12 +17,13 @@ function randomWord() {
 	var wordCount = wordArray.length;
 	var spWord = wordArray[Math.floor(Math.random()*wordCount)];
 	target.innerHTML=spWord;
-	running = true;
+	var running = true;
 	console.log(running);
 }
 
 function stopRoll() {
 	clearInterval(int);
+	target.innerHTML="";
 }
 
 
